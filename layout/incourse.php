4b9842062249955f4dfceecf4d4a6d43048024f9
matheu46 <mathias.matheus@ymail.com>
2023-08-21 14:59:15 -0300

    fazer o menu secundário aparecer no modo estudante issue #9

--- layout/incourse.php
+++ layout/incourse.php
@@ -71,16 +71,16 @@ if (!$courseindex) {
 
 $forceblockdraweropen = $OUTPUT->firstview_fakeblocks();
 
-$secondarynavigation = false;
+$secondarynavigation = true;
 $overflow = '';
 if ($PAGE->has_secondary_navigation()) {
     $secondary = $PAGE->secondarynav;
 
+    $extraclasses[] = 'has-secondarynavigation';
     if ($secondary->get_children_key_list()) {
         $tablistnav = $PAGE->has_tablist_secondary_navigation();
         $moremenu = new \core\navigation\output\more_menu($PAGE->secondarynav, 'nav-tabs', true, $tablistnav);
         $secondarynavigation = $moremenu->export_for_template($OUTPUT);
-        $extraclasses[] = 'has-secondarynavigation';
     }
 
     $overflowdata = $PAGE->secondarynav->get_overflow_menu_data();
